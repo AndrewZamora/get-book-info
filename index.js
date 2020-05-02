7576e7866806d696622a3ced8f2e95c692dd4237
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-05-02 23:48:51 +0900

    imported csv file

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const { googleBooksApiKey } = require('./vars.json');
 const axios = require('axios');
+const fs = require('fs');
+
+const readFile = (...args) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(...args, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
 const getBookInfo = async (title) => {
     const { data } = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${title}`).catch(error => console.log(error));
     return data;
 }
 
 (async () => {
-    const bookInfo = await getBookInfo("The Trouble With Harry");
-    console.log(bookInfo.items[1].saleInfo);
-})()
+    const bookData = await readFile('./books.csv', 'utf8').catch(error => console.log(error));
+    console.log(bookData)
+    return
+    const bookInfo = await getBookInfo("The Kill A Mocking Bird");
+    console.log(bookInfo.items[8].saleInfo);
+})();
